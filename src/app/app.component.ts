8ad943b9766c924b2a26da0b2f55240d057bc767
UmiriPedro <umiripedro@gmail.com>
2024-07-11 08:16:41 -0300

    fin seccion-26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth/services/auth.service';
+import { AuthStatus } from './auth/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'authApp';
+  private authService = inject( AuthService );
+  private router = inject( Router );
+
+  public finishedAuthCheck = computed<boolean>( () => {
+    return this.authService.authStatus() !== AuthStatus.checking;
+  });
+
+  public authStatusChangedEffect = effect(() => {
+    switch( this.authService.authStatus() ) {
+
+      case AuthStatus.checking:
+        return;
+
+      case AuthStatus.authenticated:
+        const url = localStorage.getItem('url');
+        if ( url ) this.router.navigateByUrl(url);
+        return;
+
+      case AuthStatus.notAuthenticated:
+        this.router.navigateByUrl('/auth/login');
+        return;
+    }
+
+  });
 }
